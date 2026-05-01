0a1bbfcc0535f16b92ebe04ff896b742a6e3ce3a
Ziko-20 <ziko20@users.noreply.github.com>
2026-05-01 15:13:37 +0100

    login/register pages

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,47 +1,61 @@
 import {React,useState} from 'react'
 import{Mail,KeyRound} from 'lucide-react';
 import logoSmall from '../assets/logoSmall.png';
+import BgRemovedLogooo from '../assets/BgRemovedLogooo.jpeg';
+import { useTranslation } from 'react-i18next';
+
 const Login = () => {
+  const { t } = useTranslation();
+
     const [email,setEmail]=useState('');
   const [password,setPassword]=useState('');
   return (
     <div className='flex h-screen w-full bg-gray-100'>
       {/* Left Side */}
-      <div className='w-1/2'>
-        <img src={logoSmall} alt="Logo" className='hidden md:block w-full h-full object-cover'/>
+      <div className='hidden sm:block  w-1/2 bg-white'>
+        <img src={logoSmall} alt="Logo" className='hidden md:block w-full h-full object-cover  '/>
         </div>
 
 
         {/* Right Side */}
-      <div className='w-1/2 flex flex-col justify-center items-center'>
+      <div className='sm:w-1/2 flex flex-col justify-center items-center  w-full sm:items-center sm:justify-center sm:bg-gray-100 '>
 
-<form className='flex flex-col items-center gap-6 bg-white p-10 rounded-2xl shadow-md  '>
+      {/* Logo Mobile */}
+{/* <img src={BgRemovedLogooo} alt="logoMobile" className=' sm:hidden'/>
+ */}
+<form className='flex flex-col items-center gap-6 bg-white p-10 rounded-2xl shadow-md sm:w-full sm:max-w-sm '>
+
+
 
   {/* EMAIL */}
-  <div className='flex items-center gap-2 w-full'>
-    <div className='flex items-center gap-2 w-[128px]'>
-      <Mail size={16}/><label>Email:</label>
+  <div className='flex flex-col   gap-2 w-full'>
+    <div className='flex items-center gap-2 w-[350px]'>
+      <Mail size={12} className='text-gray-500'/><label className='text-sm text-gray-500'>{t('email')}:</label>
     </div>
     <div className='flex-1'>
-      <input type="text" className='w-full border rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400'
+      <input type="text" className='w-full border  rounded-lg px-3 py-2 text-sm border-gray-200  focus:border-blue-600 outline-none'
         placeholder="exemple@email.com"/>
     </div>
   </div>
 
+
+
   {/* Password */}
-  <div className='flex items-center gap-2 w-full'>
-    <div className='flex items-center gap-2 w-[128px]'>
-      <KeyRound size={16}/><label>Mot de passe:</label>
+  <div className='flex flex-col   gap-2 w-full'>
+    <div className='flex items-center gap-2 w-[350px]'>
+      <KeyRound size={12} className='text-gray-500'/><label  className='text-sm text-gray-500'>{t('password')}:</label>
     </div>
     <div className='flex-1'>
-      <input type="text" className='w-full border rounded-lg px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-400'
+      <input type="text" className='w-full border rounded-lg px-3 py-2 text-sm    border-gray-200  focus:border-blue-600 outline-none'
         placeholder="••••••••"/>
     </div>
   </div>
 
+
+
   {/* Button */}
-  <button className='w-full bg-blue-400 hover:bg-blue-700 text-white font-medium py-2 rounded-lg transition-colors'>
-    S'enregistrer
+  <button className='w-full bg-blue-400 hover:bg-blue-700 text-white font-medium py-2 rounded-lg '>
+    {t('login')}
   </button>
 
 </form>
--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import{User,Mail,MapPinHouse,Phone} from 'lucide-react';
+import logoSmall from '../assets/logoSmall.png'
+
 const Register = () => {
+
+const{t}=useTranslation();
 
   const [email,setEmail]=useState('');
   const [telephone,setTelephone]=useState('');
@@ -8,40 +13,84 @@ const Register = () => {
   const [adresse,setAdresse]=useState('');
  
 
+  /* Indicatifs Telephone */
+  const countries = [
+  { code: "+212", flag: "🇲🇦", name: "Maroc" },{ code: "+1",   flag: "🇺🇸", name: "États-Unis" },{ code: "+33",  flag: "🇫🇷", name: "France" },{ code: "+44",  flag: "🇬🇧", name: "Royaume-Uni" },{ code: "+49",  flag: "🇩🇪", name: "Allemagne" },{ code: "+34",  flag: "🇪🇸", name: "Espagne" },{ code: "+39",  flag: "🇮🇹", name: "Italie" },{ code: "+32",  flag: "🇧🇪", name: "Belgique" },{ code: "+41",  flag: "🇨🇭", name: "Suisse" },{ code: "+31",  flag: "🇳🇱", name: "Pays-Bas" },{ code: "+351", flag: "🇵🇹", name: "Portugal" },{ code: "+216", flag: "🇹🇳", name: "Tunisie" },{ code: "+213", flag: "🇩🇿", name: "Algérie" },{ code: "+20",  flag: "🇪🇬", name: "Égypte" },{ code: "+221", flag: "🇸🇳", name: "Sénégal" },{ code: "+225", flag: "🇨🇮", name: "Côte d'Ivoire" },{ code: "+237", flag: "🇨🇲", name: "Cameroun" },{ code: "+966", flag: "🇸🇦", name: "Arabie Saoudite" },{ code: "+971", flag: "🇦🇪", name: "Émirats Arabes Unis" },{ code: "+974", flag: "🇶🇦", name: "Qatar" },{ code: "+965", flag: "🇰🇼", name: "Koweït" },{ code: "+962", flag: "🇯🇴", name: "Jordanie" },{ code: "+961", flag: "🇱🇧", name: "Liban" },{ code: "+90",  flag: "🇹🇷", name: "Turquie" },{ code: "+91",  flag: "🇮🇳", name: "Inde" },{ code: "+86",  flag: "🇨🇳", name: "Chine" },{ code: "+81",  flag: "🇯🇵", name: "Japon" },{ code: "+82",  flag: "🇰🇷", name: "Corée du Sud" },{ code: "+55",  flag: "🇧🇷", name: "Brésil" },{ code: "+52",  flag: "🇲🇽", name: "Mexique" },{ code: "+27",  flag: "🇿🇦", name: "Afrique du Sud" },{ code: "+234", flag: "🇳🇬", name: "Nigeria" },{ code: "+254", flag: "🇰🇪", name: "Kenya" },{ code: "+7",   flag: "🇷🇺", name: "Russie" },{ code: "+380", flag: "🇺🇦", name: "Ukraine" },{ code: "+48",  flag: "🇵🇱", name: "Pologne" },{ code: "+46",  flag: "🇸🇪", name: "Suède" },{ code: "+47",  flag: "🇳🇴", name: "Norvège" },{ code: "+45",  flag: "🇩🇰", name: "Danemark" },{ code: "+358", flag: "🇫🇮", name: "Finlande" },{ code: "+61",  flag: "🇦🇺", name: "Australie" },{ code: "+64",  flag: "🇳🇿", name: "Nouvelle-Zélande" },{ code: "+54",  flag: "🇦🇷", name: "Argentine" },{ code: "+56",  flag: "🇨🇱", name: "Chili" },{ code: "+57",  flag: "🇨🇴", name: "Colombie" },{ code: "+58",  flag: "🇻🇪", name: "Venezuela" },{ code: "+51",  flag: "🇵🇪", name: "Pérou" },
+];
+
   return (
-    <div>
-      <form action="">
+    <div className='w-full flex h-screen '>
+
+
+{/* LeftSide */}
+      <div className='w-1/2 bg-gray-100 h-full'>
+      <form action="" className='flex  flex-col items-center justify-center h-full gap-6'>
  {/* NOM */}
- <div>
- <User/>
-        <label htmlFor="">Nom:</label>
+ <div className='flex flex-col gap-2 w-1/2 '>
+  <div className='flex gap-2 items-center'><User  size={14} className=' text-gray-500'/><label htmlFor="" className='text-sm text-gray-500'>{t('name')}:</label></div>
+        
         <input type="text" 
+        className='border border-gray-300 rounded-lg hover:border-blue-600 focus:border-blue-600 outline-none pl-2 p-1'
         />
     </div>
 {/* EMAIL */}
-<div>
-  <Mail />
-        <label htmlFor="">Email</label>
+<div className='flex flex-col gap-2 w-1/2 '>
+  <div className='flex gap-2 items-center'><Mail  size={14} className=' text-gray-500' /><label htmlFor="" className='text-sm text-gray-500'>{t('email')}:</label></div>
+        
         <input type="text" 
+        className='border border-gray-300 rounded-lg hover:border-blue-600 focus:border-blue-600 outline-none pl-2 p-1'
+        placeholder='exemple@gmail.com'
         /></div>
 {/* TELEPHONE */}
-<div>
-  <Phone />
-        <label htmlFor="">Telephone</label>
-        <input type="text" 
-        /></div>
+
+         <div className="flex w-[50%]">
+<div className='flex flex-col '>
+   <div><Phone  size={14} className=' text-gray-500' /> <label htmlFor="" className='text-sm text-gray-500'>{t('phone')}:</label> </div>
+
+    <div className='flex flex-row w-full'>
+  <select
+    name="indicatif"
+    className='border border-gray-300 rounded-lg hover:border-blue-600 focus:border-blue-600 outline-none pl-2 p-1'
+    defaultValue="+212"
+  >
+    {countries.map((c) => (
+      <option key={c.code} value={c.code}>
+        {c.flag} {c.code}
+      </option>
+    ))}
+  </select>
+
+    
+  <input
+    type="tel"
+    name="telephone"
+    placeholder="  6 12 34 56 78"
+    className='border border-gray-300 rounded-lg hover:border-blue-600 focus:border-blue-600 outline-none w-full pl-2 p-1'
+  /></div></div>
+</div>
+        
 {/* ADRESSE */}
-<div>
-  <MapPinHouse />
-        <label htmlFor="">Aresse</label>
+<div className='flex flex-col gap-2 w-1/2 '>
+  <div className='flex gap-2 items-center'><MapPinHouse  size={14} className=' text-gray-500' /><label htmlFor="" className='text-sm text-gray-500'>{t('adress')}:</label></div>
+        
         <input type="text" 
+        className='border border-gray-300 rounded-lg  hover:border-blue-600 focus:border-blue-600 outline-none pl-2 p-1'
+        placeholder='12 Rue Al Atlas, Appartement 5 , Rabat'
         /></div>
 
 {/* Button */}   
-<div>    
-        <button /* onClick={submit} */>S'enregistrer</button>
+<div className='w-[50%] flex justify-center'>    
+        <button /* onClick={submit} */ className='border  bg-blue-600 text-white font-bold w-full rounded-lg pl-2 p-1 hover:bg-blue-400'><a href="#">{t('Register')}</a></button>
         </div> 
         </form>
+        </div>
+
+
+
+        {/* Right Side */}
+<div className='hidden  w-1/2 h-full sm:block bg-white '>
+   <a href="#"><img src={logoSmall} alt="Logo" className='hidden md:block w-full h-full object-cover transition-transform duration-300 hover:scale-110'/></a> 
+</div>
     </div>
   )
 }
